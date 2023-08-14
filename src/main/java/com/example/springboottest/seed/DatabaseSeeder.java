2f3a4de09f38a126cf6b329f2d5f533c1cdd1892
AbdulrahmanShater <106619258+AbdulrahmanShater@users.noreply.github.com>
2023-08-14 19:37:33 +0400

    add seeder for testing database and have example values

--- src/main/java/com/example/springboottest/seed/DatabaseSeeder.java
+++ src/main/java/com/example/springboottest/seed/DatabaseSeeder.java
@@ -31,17 +31,44 @@ public class DatabaseSeeder implements CommandLineRunner {
     @Override
     public void run(String... args) {
 
-        AddressType addressType = new AddressType();
-        addressType.setName("Home");
-        addressTypeRepository.save(addressType);
+        AddressType homeType = new AddressType();
+        homeType.setName("Home");
+        addressTypeRepository.save(homeType);
 
-        Country country = new Country();
-        country.setName("UAE");
-        countryRepository.save(country);
+        AddressType workType = new AddressType();
+        workType.setName("Work");
+        addressTypeRepository.save(workType);
 
-        City city = new City();
-        city.setName("Abu Dhabi");
-        city.setCountry(country);
-        cityRepository.save(city);
+
+        AddressType pob = new AddressType();
+        pob.setName("POB");
+        addressTypeRepository.save(pob);
+
+
+        Country uae = new Country();
+        uae.setName("UAE");
+        countryRepository.save(uae);
+
+        Country syria = new Country();
+        syria.setName("Syria");
+        countryRepository.save(syria);
+
+        City abuDhabi = new City();
+        abuDhabi.setName("Abu Dhabi");
+        abuDhabi.setCountry(uae);
+        cityRepository.save(abuDhabi);
+
+
+        City dubai = new City();
+        dubai.setName("Dubai");
+        dubai.setCountry(uae);
+        cityRepository.save(dubai);
+
+        City aleppo = new City();
+        aleppo.setName("Aleppo");
+        aleppo.setCountry(syria);
+        cityRepository.save(aleppo);
+
+
     }
 }
