4d3842d538ef2ee68a2a821602b4e4dfb3b6bd38
AbdulrahmanShater <106619258+AbdulrahmanShater@users.noreply.github.com>
2023-08-14 19:10:59 +0400

    move phone and city api to customer controller

--- src/main/java/com/example/springboottest/controller/CustomerController.java
+++ src/main/java/com/example/springboottest/controller/CustomerController.java
@@ -12,7 +12,6 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/customer")
 public class CustomerController {
     final CustomerService customerService;
 
@@ -20,30 +19,39 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @PostMapping
+    @PostMapping("/customer")
     public ResponseEntity<DataResponse<CustomerDTO>> createCustomer(@RequestBody @Valid CustomerDTO customerDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(new DataResponse<>("success", "Customer created successfully.", customerService.save(customerDTO)));
     }
 
-    @GetMapping
+    @GetMapping("/customer")
     public ResponseEntity<DataResponse<List<CustomerDTO>>> getCustomers() {
         return ResponseEntity.status(HttpStatus.OK).body(new DataResponse<>("success", "Customers retrieved successfully.", customerService.getCustomers()));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/customer/{id}")
     public ResponseEntity<DataResponse<CustomerDTO>> getCustomer(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(new DataResponse<>("success", "Customer retrieved successfully.", customerService.getCustomerById(id)));
     }
 
-    @PostMapping("/{id}/address")
+    @PostMapping("/customer/{id}/address")
     public ResponseEntity<DataResponse<CustomerDTO>> addCustomerAddress(@PathVariable Long id, @Valid @RequestBody AddressDTO addressDto) {
         return ResponseEntity.status(HttpStatus.OK).body(new DataResponse<>("success", "Address added successfully.", customerService.addAddress(id, addressDto)));
     }
 
-    @DeleteMapping("/{id}/address/{addressId}")
+    @DeleteMapping("/customer/{id}/address/{addressId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteAddress(@PathVariable Long id, @PathVariable Long addressId) {
         customerService.deleteAddress(id, addressId);
     }
 
+    @GetMapping("city/{name}")
+    public List<CustomerDTO> getCustomerByCities(@PathVariable String name) {
+        return customerService.getCustomersByCity(name);
+    }
+
+    @GetMapping("phone/{prefix}")
+    public List<CustomerDTO> getByPhone(@PathVariable String prefix) {
+        return customerService.getCustomersByPhone(prefix);
+    }
 }
